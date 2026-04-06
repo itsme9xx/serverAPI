80d3f33a917b64fc0d549996dfec195e8973098c
itsme9xx <hoanghai121298.dhtl@gmail.com>
2026-04-07 01:38:07 +0700

    add api song ytb

--- controllers/YoutubeController.js
+++ controllers/YoutubeController.js
@@ -1,4 +1,4 @@
-const { exec } = require("child_process");
+const { execFile } = require("child_process");
 
 class YoutubeController {
   async search(req, res) {
@@ -28,6 +28,46 @@ class YoutubeController {
       res.json({
         data: videos,
       });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+  async getAudio(req, res) {
+    try {
+      const { videoId } = req.query;
+
+      if (!videoId) {
+        return res.status(400).json({ message: "Missing videoId" });
+      }
+
+      // validate videoId
+      if (!/^[a-zA-Z0-9_-]{11}$/.test(videoId)) {
+        return res.status(400).json({ message: "Invalid videoId" });
+      }
+
+      const url = `https://www.youtube.com/watch?v=${videoId}`;
+      execFile(
+        "python",
+        ["-m", "yt_dlp", "-f", "bestaudio", "-g", url],
+        { timeout: 10000 },
+        (err, stdout, stderr) => {
+          if (err) {
+            console.error(stderr);
+            return res.status(500).json({
+              message: "Không thể lấy audio",
+            });
+          }
+
+          const audioUrl = stdout.trim();
+
+          res.json({
+            success: true,
+            videoId,
+            audioUrl,
+          });
+        }
+      );
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Server error" });
--- routers/api/YoutubeRoute.js
+++ routers/api/YoutubeRoute.js
@@ -4,5 +4,6 @@ const YoutubeController = require("../../controllers/YoutubeController");
 const router = express.Router();
 
 router.get("/youtube", YoutubeController.search);
+router.get("/audio", YoutubeController.getAudio);
 
 module.exports = router;
